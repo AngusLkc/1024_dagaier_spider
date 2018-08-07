9c2c922fce1ebfa764f0539f08fb9f5b43833c2e
cary.zhou <kaichuangluo@gmail.com>
2018-08-08 01:30:14 +0800

    解决Windows控制台日志输出乱码问题

--- 达盖尔.py
+++ 达盖尔.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python2.7
-# -*- coding: utf-8
+# -*- coding: utf-8 -*-
 
 import os
 import sys
 reload(sys)
-sys.setdefaultencoding('utf-8')
+sys.setdefaultencoding('utf8')
 import time
 import inspect
 import logging
@@ -24,6 +24,7 @@ header={
     "Accept-Encoding":"gzip, deflate",
     "Connection":"close"
 }
+
 '''国内抓取需配置代理'''
 proxy={"http":"socks5://127.0.0.1:1088","https":"socks5://127.0.0.1:1088"}
 
@@ -39,7 +40,7 @@ class ThreadManager(object):
     
     def __start__(self):
         for i in range(self.thread_num):
-            i=ThreadWork(self.queue,self.shutdown,i) #i:值传递
+            i=ThreadWork(self.queue,self.shutdown,i)
             i.start()
             self.threadlist.append(i)
     
@@ -65,7 +66,7 @@ class ThreadWork(threading.Thread):
     def run(self):
         while True:
             if self.shutdown.isSet():
-                logging.info("线程ID：%s，检测到线程退出标志！"%(self.getName()))
+                logging.info(u"线程ID：%s，检测到线程退出标志！"%(self.getName()))
                 break
             try:
                 url,title=self.tasklist.get(timeout=1)
@@ -83,7 +84,7 @@ def dagaier(topicurl,title):
     topic_req=req.get(topicurl,timeout=5)
     topic_req.encoding='gbk'
     if topic_req.status_code!=200:
-        logging.warning("线程ID：%s，下载主题页失败, Status:%s, URL:%s"%(threading.currentThread().getName(),topic_req.status_code,topicurl))
+        logging.warning(u"线程ID：%s，下载主题页失败, Status:%s, URL:%s"%(threading.currentThread().getName(),topic_req.status_code,topicurl))
         return False
     topic_pq=pq(topic_req.text)
     imglist=topic_pq("div[class='tpc_content do_not_catch']>input[type='image']").items()
@@ -93,7 +94,7 @@ def dagaier(topicurl,title):
         elif item.attr('data-src') is not None:
             downimg(item.attr('data-src'),title)
         else:
-            logging.warning("线程ID：%s，读取帖子内图片URL失败, URL:%s"%(threading.currentThread().getName(),topicurl))
+            logging.warning(u"线程ID：%s，读取帖子内图片URL失败, URL:%s"%(threading.currentThread().getName(),topicurl))
             return False
 
 def downimg(url,title):
@@ -114,12 +115,12 @@ def downimg(url,title):
             fd.write(img_req.content)
         return True
     else:
-        logging.warning("线程ID:%s，下载图片失败, Status:%s, URL:%s"%(threading.currentThread().getName(),img_req.status_code,url))
+        logging.warning(u"线程ID:%s，下载图片失败, Status:%s, URL:%s"%(threading.currentThread().getName(),img_req.status_code,url))
         return False
 
 if __name__=='__main__':
     os.chdir(os.path.dirname(os.path.realpath(inspect.getfile(inspect.currentframe()))))
-    work_manager=ThreadManager(10) #线程数
+    work_manager=ThreadManager(8) #线程数
     work_manager.__start__()
     req=requests.session()
     req.headers.update(header)
@@ -129,10 +130,10 @@ if __name__=='__main__':
     BasicURL='http://t66y.com/'
     offset=0
     error_count=0
-    while offset<1: #主题列表页数
+    while offset<10: #主题列表分页数
         offset+=1
         if error_count>=3:
-            logging.error("遍历主题列表页失败！页码:%i"%(offset))
+            logging.error(u"遍历主题列表页失败！页码:%i"%(offset))
             error_count=0
             continue
         PageList='http://t66y.com/thread0806.php?fid=16&search=&page='+str(offset)
@@ -140,7 +141,7 @@ if __name__=='__main__':
         Page_Obj.encoding='gbk'
         if Page_Obj.status_code!=200:
             error_count+=1
-            logging.warn("下载主题列表分页失败：%i，重试:%i"%(offset,error_count))
+            logging.warn(u"下载主题列表分页失败：%i，重试:%i"%(offset,error_count))
             offset-=1
             continue
         error_count=0
@@ -151,8 +152,8 @@ if __name__=='__main__':
                 work_manager.add_task(BasicURL+i.attr('href'),i.text())
     while not work_manager.isEmpty():
         time.sleep(1)
-    logging.info("设置程序关闭标志")
+    logging.info(u"设置程序关闭标志")
     work_manager.__close__()
-    logging.info("等待所有线程退出")
+    logging.info(u"等待所有线程退出")
     work_manager.waitcomplete()
     sys.exit(0)
